3f13887822e30c80795d8245cc0cce6bd93e8081
David Light <dlight@beanstream.com>
2017-03-22 11:02:27 -0700

    PAY-1230 Automatically download swagger spec for Payments API

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -1,4 +1,4 @@
-module CustomHelpers 
+module CustomHelpers
 
   # Display svg images inline
   # optionally pass a string to use as the html class(es)
@@ -7,7 +7,7 @@ module CustomHelpers
     file_path = "#{root}/source/images/svg/#{name}.svg"
     if File.exists?(file_path)
       f = File.read(file_path)
-      if classes.any? 
+      if classes.any?
         return f.gsub('<svg ', "<svg class='#{classes.join(' ')}'")
       else
         return f
@@ -17,10 +17,10 @@ module CustomHelpers
     end
   end
 
-  # Return random color for card icon. 
+  # Return random color for card icon.
   # Names must exist in Bamora UI as classes.
   def get_random_icon_color()
-    ['green-light', 'raspberry', 'tabriz-blue', 
+    ['green-light', 'raspberry', 'tabriz-blue',
       'green-powder', 'factory-yellow'].sample
   end
 
@@ -30,9 +30,9 @@ module CustomHelpers
     "#{github_repo_url}/edit/#{github_branch}/source/#{path}"
   end
 
-  # Returns href to edit partial on github 
+  # Returns href to edit partial on github
   def github_edit_include_link(partial)
-    path, _, file = partial.rpartition('/')    
+    path, _, file = partial.rpartition('/')
     "#{github_repo_url}/edit/#{github_branch}/source/includes/#{path}/_#{file}.md"
   end
 
@@ -41,13 +41,13 @@ module CustomHelpers
   def get_breadcrumbs(path)
     result = {}
     path_list = path.split('/')
-    
+
     if path_list.last == "index.html"
       path_list = path_list[0...-1] # Don't include current page (index.html) or directory/
     else
-        path_list = path_list[0...-1] << File.basename(path_list.last, '.html')  
+        path_list = path_list[0...-1] << File.basename(path_list.last, '.html')
     end
-    
+
     path_list.each_with_index do |path, index|
       link = "/" + path_list[0..index].join('/') + '/'
       result[link] = path
@@ -55,22 +55,48 @@ module CustomHelpers
     result
   end
 
-  # Render markdown text to html tags 
+  # Render markdown text to html tags
   def render_markdown(md)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     markdown.render(md)
   end
 
-  # Return the current page path in the form required for 
-  # the side nav toc. 
+  # Return the current page path in the form required for
+  # the side nav toc.
   def get_page_path(current_page)
     "/" + current_page.path.split('.').first + "/"
   end
 
-  # Downloads the specified swagger file at build time 
-  # Can then refer to the destination file name in a 
-  # swagger template to render the spec.  
-  def get_swagger_doc(url, destination)
+
+  # Uses the swagger endpoint to get a list of specs
+  # Can then refer to the destination file name in a
+  # swagger template to render the spec.
+  def get_all_swagger_specs(url)
+    require "json"
+    require "net/http"
+
+    swaggerUrl = "#{url}/swagger"
+
+    uri = URI(swaggerUrl)
+    response = Net::HTTP.get uri
+
+    result = JSON.parse response
+    if result.is_a?(Array)
+      result.each do |spec|
+        id = spec["id"]
+        href = spec["href"]
+        get_swagger_spec "#{url}/#{href}", "na/API_spec/merchant/#{id}.yaml"
+      end
+    else
+      # do it the old way
+      get_swagger_spec "#{swaggerUrl}", "na/API_spec/merchant/1-0-2.json"
+    end
+  end
+
+  # Downloads the specified swagger file at build time
+  # Can then refer to the destination file name in a
+  # swagger template to render the spec.
+  def get_swagger_spec(url, destination)
     require 'open-uri'
     require 'fileutils'
     root = Middleman::Application.root
@@ -81,38 +107,38 @@ module CustomHelpers
     IO.copy_stream(download, "#{root}/data/autodownload/#{destination}")
   end
 
-  # Get the data file referenced using dot notation 
-  # from the front matter of a page. 
-  def get_data_from_frontmatter(path) 
+  # Get the data file referenced using dot notation
+  # from the front matter of a page.
+  def get_data_from_frontmatter(path)
     path = path.split('.')
     result = data
     path.each do |p|
       result = result["#{p}"]
     end
-    result   
-  end 
+    result
+  end
 
   # Nicely format a string for use as a html id or other attribute.
-  def make_html_safe(str) 
+  def make_html_safe(str)
     str.gsub(/[^-\p{Alnum}]/, '')
-  end 
+  end
 
-  # Like titleize(), but better because it will keep all caps words in 
+  # Like titleize(), but better because it will keep all caps words in
   # the name, e.g. test_API will become Test API (not Test Api).
   def breadcrumb_titleize(name)
     File.basename(name, '.html').gsub(/_/, ' ').split.map{|x| x.slice(0,1).capitalize + x.slice(1..-1)}.join(' ')
   end
 
 
-  # Build div of breadcrumbs from get_breadcrumbs. Not great to define HTML 
-  # inline like this but needed at build time when creating the search index.  
+  # Build div of breadcrumbs from get_breadcrumbs. Not great to define HTML
+  # inline like this but needed at build time when creating the search index.
   def format_breadcrumb_trail(breadcrumbs, div_class: '')
     result = "<div class='#{div_class}'>"
-    breadcrumbs.each_with_index do |(link, name), index| 
-      if index != breadcrumbs.length - 1 
+    breadcrumbs.each_with_index do |(link, name), index|
+      if index != breadcrumbs.length - 1
         result += "<span><a href='#{link}'>#{breadcrumb_titleize name}</a></span>"
         result += "<span class='chevron'> / </span>"
-      else 
+      else
         result += "<span class='this-page'>#{breadcrumb_titleize name}</span>"
       end
     end
@@ -122,18 +148,18 @@ module CustomHelpers
 
   def get_swagger_operations_by_tag(paths)
     result = {}
-    paths.each do |path| 
+    paths.each do |path|
       if path[0].start_with?('/') # swagger path item object
         path_name = path[0]
         operations = path[1]
 
-        operations.each do |operation| 
+        operations.each do |operation|
           operation_name = operation[0]
           operation_body = operation[1]
 
-          if ['get', 'put', 'post', 'delete', 
+          if ['get', 'put', 'post', 'delete',
               'options', 'head', 'path'].include? operation_name
-            # ( else an external ref or list of parameters, 
+            # ( else an external ref or list of parameters,
             #   not currently handled. )
             if operation_body.has_key? "tags"
               tags = operation_body.tags
@@ -143,12 +169,12 @@ module CustomHelpers
               result_object["operation_name"] = operation_name
               result_object["operation_body"] = operation_body
 
-              tags.each do |tag| 
+              tags.each do |tag|
                 if result.has_key? tag
                   result[tag] << result_object
-                else 
+                else
                   result[tag] = [result_object]
-                end  
+                end
               end
             end
           end
@@ -158,7 +184,7 @@ module CustomHelpers
     return result
   end
 
-  # Return the hash of the latest git commit. Included on each page as 
+  # Return the hash of the latest git commit. Included on each page as
   # the site version.
   def get_latest_commit()
     `git log -1 HEAD --format=%H`
